11d149a500b13ce9f7e5d356d3b77be3939157ed
ukm01 <94747360+ukm01@users.noreply.github.com>
2024-08-17 21:00:25 +0530

    minor change

--- netflix-gpt/src/Components/Login.jsx
+++ netflix-gpt/src/Components/Login.jsx
@@ -16,7 +16,7 @@ const Login = () => {
         <form className=' w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80'>
            <h1 className='font-bold text-3xl py-4'>{isSignInForm? "Sign In":"Sign Up"}</h1>
            {!isSignInForm && (<input
-             type="text" placeholder=' Full Name'
+             type="text" placeholder='Name'
               className="p-4 my-4 w-full bg-gray-700"/>)}
             <input
              type="text" placeholder='Email Address' 
